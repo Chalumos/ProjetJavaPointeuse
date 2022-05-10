daab2439eb0d7d0b9fb975fdac25136d7965cbbc
Theo Mendes <theo200087@gmail.com>
2022-05-10 12:32:14 +0200

    ci: Ajout de la classe Reporting et TimeClock

--- src/helloWord/HelloWord.java
+++ src/helloWord/HelloWord.java
@@ -2,6 +2,6 @@ package helloWord;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Hello World !");
     }
 }
---
+++ src/reporting_timeClock/TimeClock.java
@@ -0,0 +1,5 @@
+package reporting_timeClock;
+
+public class TimeClock {
+
+}
