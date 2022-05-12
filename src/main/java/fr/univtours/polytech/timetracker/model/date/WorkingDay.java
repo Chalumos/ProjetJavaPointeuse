237c458f9c94342d97ed9a832c70cd0d28995edc
Theo Mendes <theo200087@gmail.com>
2022-05-12 12:31:01 +0200

    ci: Ajout de la classe WorkingDay et EmployeeCard

--- src/main/java/fr/univtours/polytech/timetracker/model/date/WorkingDay.java
+++ src/main/java/fr/univtours/polytech/timetracker/model/date/WorkingDay.java
@@ -1,33 +1,79 @@
 package fr.univtours.polytech.timetracker.model.date;
 
-public class WorkingDay {
+public class WorkingDay
+{
+    /**
+     * Day of work.
+     */
     private Days day;
+
+    /**
+     * Time when the employee has supposed to start working.
+     */
     private Time startTime;
+
+    /**
+     * Time when the employee has supposed to stop working.
+     */
     private Time endTime;
 
-
-    //Methods
+    /**
+     * Return the day of work.
+     * @return Day of work.
+     */
     public Days getDay() {
         return day;
     }
 
+    /**
+     * Set the day of work.
+     * @param day The day of work.
+     */
     public void setDay(Days day) {
         this.day = day;
     }
 
+    /**
+     * Return the time when the employee has supposed to start working.
+     * @return The time when the employee has supposed to start working.
+     */
     public Time getStartTime() {
         return startTime;
     }
 
+    /**
+     * Set the time when the employee has supposed to start working
+     * @param startTime The time when the employee has supposed to start working.
+     */
     public void setStartTime(Time startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * Return the time when the employee has supposed to stop working.
+     * @return The time when the employee has supposed to stop working.
+     */
     public Time getEndTime() {
         return endTime;
     }
 
+    /**
+     * Set the time when the employee has supposed to stop working
+     * @param endTime The time when the employee has supposed to stop working.
+     */
     public void setEndTime(Time endTime) {
         this.endTime = endTime;
+    }
+
+    /**
+     * Return the period of time when an employee is supposed to work in the day.
+     * @return The period of time when an employee is supposed to work in the day.
+     */
+    @Override
+    public String toString() {
+        String message = "Date : " + getDay()
+                + "\n\tStarting time : " + getStartTime()
+                + "\n\tEnding time : " + getEndTime();
+        return message;
     }
 }
--- src/main/java/fr/univtours/polytech/timetracker/model/employee/EmployeeCard.java
+++ src/main/java/fr/univtours/polytech/timetracker/model/employee/EmployeeCard.java
@@ -1,13 +1,42 @@
 package fr.univtours.polytech.timetracker.model.employee;
 
-public class EmployeeCard {
-    private String id;
+public class EmployeeCard
+{
+    /**
+     * Employee
+     */
+    private Employee employee;
 
-    public EmployeeCard() {
-        this.id = "";
+    /**
+     * Create an employee card which initialize the attributes to null.
+     */
+    public EmployeeCard()
+    {
+        this.employee.setId(null);
+        this.employee.setFirstName(null);
+        this.employee.setLastName(null);
+        this.employee.setDepartment(null);
     }
 
-    public EmployeeCard(String id) {
-        this.id = id;
+    /**
+     * Create an employee card which initialize the attribute by the attributes of the employee in parameter.
+     * @param employee An employee.
+     */
+    public EmployeeCard(Employee employee)
+    {
+        this.employee.setId(employee.getId());
+        this.employee.setFirstName(employee.getFirstName());
+        this.employee.setLastName(employee.getLastName());
+        this.employee.setDepartment(employee.getDepartment());
+    }
+
+    /**
+     * Return a description of the owner card.
+     * @return A description of the owner card.
+     */
+    @Override
+    public String toString() {
+        String message = "The card belong to " + employee.getFirstName() + " " + employee.getLastName() + " [ " + employee.getId() + " ]" ;
+        return message;
     }
 }
