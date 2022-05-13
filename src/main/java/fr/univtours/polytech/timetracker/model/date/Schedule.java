64674504ed998fff19ea18c6cece97fefaf55666
Aurel <aurelien.dafonseca@gmail.com>
2022-05-13 11:10:09 +0200

    implémentation de toString dans schedule

--- src/main/java/fr/univtours/polytech/timetracker/model/date/Schedule.java
+++ src/main/java/fr/univtours/polytech/timetracker/model/date/Schedule.java
@@ -1,9 +1,12 @@
 package fr.univtours.polytech.timetracker.model.date;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Schedule {
     private int weekNumber;
-    private Date date;
-    private WorkingDay workingDays;
+    private Date weekDate;
+    private Map<Days,WorkingDay> workingDays=new HashMap<Days,WorkingDay>();
 
     public int getWeekNumber() {
         return weekNumber;
@@ -13,19 +16,65 @@ public class Schedule {
         this.weekNumber = weekNumber;
     }
 
-    public WorkingDay getWorkingDays() {
+    public Date getWeekDate() {
+        return weekDate;
+    }
+
+    public void setWeekDate(Date weekDate) {
+        this.weekDate = weekDate;
+    }
+
+    public Map<Days, WorkingDay> getWorkingDays() {
         return workingDays;
     }
 
-    public void setWorkingDays(WorkingDay workingDays) {
-        this.workingDays = workingDays;
-    }
+    public String toString(){
+        String message= "this employee is scheduled to work :\n";
+        for (Days day:workingDays.keySet()){
+            if(day.equals(Days.SATURDAY) || day.equals(Days.SUNDAY)){
+                message+="break for "+workingDays.get(day).toString();
+            }
+            else{
+                message+="between "+workingDays.get(day).getStartTime().toString()
+                        +" and "+workingDays.get(day).getEndTime().toString();
+                message+=" on "+workingDays.get(day).toString()+"\n";
+            }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
+        }
+        message+="during the week number "+weekNumber+" of the year "+weekDate.getYear();
+        return message;
     }
 }
+
+class testSchedule{
+    public static void main(String[] args){
+        Schedule S1 = new Schedule();
+
+        Date today =new Date();
+        today.setDay(13);
+        today.setYear(2022);
+        S1.setWeekDate(today);
+
+        Time start =new Time();
+        Time end=new Time();
+
+        start.setHour(8);
+        start.setMinute(00);
+
+        end.setHour(18);
+        end.setMinute(30);
+
+
+
+        for (Days day: Days.values()){
+            WorkingDay w =new WorkingDay();
+            w.setDay(day);
+            w.setStartTime(start);
+            w.setEndTime(end);
+            S1.getWorkingDays().put(day,w);
+        }
+        //System.out.println(S1.getWorkingDays().get(Days.MONDAY));
+        System.out.println(S1.toString());
+    }
+}
+
