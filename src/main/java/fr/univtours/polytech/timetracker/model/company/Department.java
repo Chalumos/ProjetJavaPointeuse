36bdf5669314b7e479a71ba62c3e53054838b36b
Chalumos <charlohuberta@gmail.com>
2022-05-13 10:51:59 +0200

    Javadoc de la classe Employee, Company et Department

--- src/main/java/fr/univtours/polytech/timetracker/model/company/Department.java
+++ src/main/java/fr/univtours/polytech/timetracker/model/company/Department.java
@@ -5,24 +5,53 @@ import fr.univtours.polytech.timetracker.model.employee.Employee;
 import java.util.ArrayList;
 
 public class Department {
+    /**
+     * Department name
+     */
     private String name;
+    /**
+     * List of employee of a department
+     */
     private ArrayList<Employee> employees;
 
+    /**
+     * Default constructor
+     */
     public Department() {
         this.name = "";
         this.employees = new ArrayList<Employee>();
     }
 
+    /**
+     * Constructor with department name
+     * @param name
+     */
     public Department(String name) {
         this.name = name;
         this.employees = new ArrayList<Employee>();
     }
 
+    /**
+     * @return the name of the department
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     * this method allows you to change the name of the department
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @return the name of department
+     */
+    @Override
+    public String toString() {
+        return "Department: " + this.name;
     }
 }
--- src/main/java/fr/univtours/polytech/timetracker/model/employee/Employee.java
+++ src/main/java/fr/univtours/polytech/timetracker/model/employee/Employee.java
@@ -1,52 +1,155 @@
 package fr.univtours.polytech.timetracker.model.employee;
 
 import fr.univtours.polytech.timetracker.model.company.Department;
+import fr.univtours.polytech.timetracker.model.date.Schedule;
 
 public class Employee {
-    private String id;
-    private String lastName;
-    private String firstName;
-    private Department department;
-    private EmployeeCard employeeCard;
-    //private Schedule schedule;
 
-    public Employee(String id, String lastName, String firstName, Department departement) {
+    /**
+     * Employee id
+     */
+    private String id;
+
+    /**
+     * Employee last name
+     */
+    private String lastName;
+    /**
+     * Employee first name
+     */
+    private String firstName;
+    /**
+     * Employee department
+     */
+    private Department department;
+    /**
+     * Employee card
+     */
+    private EmployeeCard employeeCard;
+    /**
+     * Employee Schedule
+     */
+    private Schedule schedule;
+
+    /**
+     * Comfort constructor allows you to create an employee by directly initializing all its attributes
+     * @param id
+     * @param lastName
+     * @param firstName
+     * @param department
+     */
+    public Employee(String id, String lastName, String firstName, Department department) {
         this.id = id;
         this.lastName = lastName;
         this.firstName = firstName;
-        this.department = departement;
-        this.employeeCard = new EmployeeCard(id);
+        this.department = department;
+        this.employeeCard = new EmployeeCard(this);
+        this.schedule = new Schedule();
     }
 
+    /**
+     * Get the employee id
+     * @return id of employee
+     */
     public String getId() {
         return id;
     }
 
+
+    /**
+     * Set the employee id
+     * @param id the new employee id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Get the employee last name
+     * @return last name of employee
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Set the employee lastName
+     * @param lastName the new employee lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Get the employee first name
+     * @return first name of employee
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Set the employee firstName
+     * @param firstName the new employee firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Get the employee department
+     * @return department of employee
+     */
     public Department getDepartment() {
         return department;
     }
 
+    /**
+     * Set the employee department
+     * @param department the new employee department
+     */
     public void setDepartment(Department department) {
         this.department = department;
+    }
+
+    /**
+     * Get the employee Card
+     * @return the EmployeeCard
+     */
+    public EmployeeCard getEmployeeCard() {
+        return employeeCard;
+    }
+
+    /**
+     * Set the employee card
+     * @param employeeCard the new employee card
+     */
+    public void setEmployeeCard(EmployeeCard employeeCard) {
+        this.employeeCard = employeeCard;
+    }
+
+    /**
+     * Get the employee schedule
+     * @return the Schedule of employee
+     */
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * Set the employee schedule
+     * @param schedule the new employee schedule
+     */
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    /**
+     * @return Employee informations
+     */
+    @Override
+    public String toString() {
+        return "Employee : "+ lastName + " " + firstName + " ID: "+ id +
+                department.toString();
     }
 }
