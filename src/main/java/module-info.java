f735206829adcf9fcbcd4de1ee526fc703797780
Jules Pascot <jules.pascot@gmail.com>
2022-05-31 09:20:14 +0200

    ci: Mise en place de l'IHM de l'application de monitoring

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,15 +3,23 @@ module fr.univtours.polytech.timetracker {
     requires javafx.fxml;
     requires java.desktop;
 
-
+    // MVC.
     opens fr.univtours.polytech.projet_tutore to javafx.fxml;
     exports fr.univtours.polytech.projet_tutore;
     exports fr.univtours.polytech.projet_tutore.controller;
     opens fr.univtours.polytech.projet_tutore.controller to javafx.fxml;
     exports fr.univtours.polytech.projet_tutore.view;
     opens fr.univtours.polytech.projet_tutore.view to javafx.fxml;
+
+    // Time-tracker.
     exports fr.univtours.polytech.projet_tutore.view.timetracker;
     opens fr.univtours.polytech.projet_tutore.view.timetracker to javafx.fxml;
     exports fr.univtours.polytech.projet_tutore.controller.timetracker;
     opens fr.univtours.polytech.projet_tutore.controller.timetracker to javafx.fxml;
+
+    // Application.
+    exports fr.univtours.polytech.projet_tutore.view.application;
+    opens fr.univtours.polytech.projet_tutore.view.application to javafx.fxml;
+    exports fr.univtours.polytech.projet_tutore.controller.application;
+    opens fr.univtours.polytech.projet_tutore.controller.application to javafx.fxml;
 }
